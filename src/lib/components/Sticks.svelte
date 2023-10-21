9e56324192433894ca78b9bacc6b758ec9216bdd
Sebastian Laclé <info@slacle.com>
2023-10-21 21:56:41 +0200

    Add sticks

---
+++ src/lib/components/Sticks.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { T, useFrame } from "@threlte/core";
+
+  let sticks = [];
+
+  export let posY = 1;
+  export let amount = 20;
+
+  for (let i = 1; i <= amount; i++) {
+    sticks.push(i);
+  }
+
+  let change = 0;
+
+  useFrame((_, delta) => {
+    change += delta;
+  });
+</script>
+
+{#each sticks as stick}
+  <T.Mesh
+    position={[(6 / amount) * stick - 3.5, posY, 0]}
+    rotation.x={stick * change * 0.1}
+  >
+    <T.BoxGeometry args={[0.1, stick * 0.1, 0.1]} />
+    <T.MeshStandardMaterial
+      color={`hsl(${(360 / amount) * stick}, 100%, 50%)`}
+    />
+  </T.Mesh>
+{/each}
